f13944172974c6071b74e61580f68219bb7f80d5
Donghyeon <kevink3@naver.com>
2020-07-31 17:52:02 +0900

    builld project

---
+++ avoidcorona/avoidcorona/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  avoidcorona
+//
+//  Created by 김동현 on 2020/07/31.
+//  Copyright © 2020 김동현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
